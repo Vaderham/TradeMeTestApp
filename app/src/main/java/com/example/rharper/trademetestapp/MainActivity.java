e59bdfc0806efdd18e654210253f1c2ab669e571
Reagan Harper <reagan.harper@trademe.co.nz>
2018-03-27 11:31:03 +1300

    First commit

---
+++ app/src/main/java/com/example/rharper/trademetestapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.rharper.trademetestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Async call to TM API for list of categories.
+        // Async call for list of top 50 listings, site wide.
+
+        //Set up list to store returned Cats
+
+        //Set up navigation drawer
+        //Populate with list of cats
+
+        //Set up recyclerview in mainactivity
+        //Display list of listings in activity
+
+    }
+
+    
+
+
+}
